3a60228aaeeea48da27d29db8344a475b15ea41f
Yves <yvesduke06@live.co.uk>
2023-10-19 16:05:37 +0100

    Initial Commi

---
+++ ShoppingApp/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  ShoppingApp
+//
+//  Created by Yves Dukuze on 19/10/2023.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    private var items: FetchedResults<Item>
+
+    var body: some View {
+        
+        Text(" E-Commerce")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Yves Dukuze on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
